02bbff8b5f08759afd8c1c61cea5ecdac0b28081
kaiesmahmud <kaiesmahmud@gmail.com>
2023-03-08 16:25:41 +0600

    bug fixed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import { About, ContactMe, Experience, Gigs, Home, Projects } from './pages';
 //Components
 import { Navbar } from './component';
-
+//icons
+import {BsWhatsapp} from 'react-icons/bs'
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App text-[#D7CEA7]">
+     
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
@@ -19,9 +21,15 @@ function App() {
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/experience' element={<Experience/>}/>
         <Route path='/contactme' element={<ContactMe/>}/>
-
-      </Routes>
-
+         {/* Whatsapp link  */}
+        {/* <div style={{color:'green'}} className=" fixed bottom-5 right-5 md:bottom-10 md:right-10 text-[30px] rounded-[50%]  bg-white p-4 shadow-2xl"> 
+          <a href="https://wa.me/message/NYI6PJ3INOWOM1" target={'_blank'} rel="noreferrer " >
+            <BsWhatsapp />
+          </a>
+          <div className=' absolute top-[5%] right-[5%] w-3 h-3 bg-red-500 rounded-[50%]  animate-ping-smooth duration-[5s]'> </div>
+        </div> */}
+        </Routes>
+      
     </div>
   );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -39,6 +39,7 @@ const Navbar = () => {
                         </NavLink>
                     </div>
                 </nav>
+                
             </header>
         </>
     );
